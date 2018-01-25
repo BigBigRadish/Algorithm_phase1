ac30d92883c5fa0e5bf57f98c9e9a2e2fb5f77f6
radish <luozhukun@163.com>
2018-01-25 21:11:46 +0800

    Update linkedStack.java

--- src/stack/linkedStack.java
+++ src/stack/linkedStack.java
@@ -5,7 +5,11 @@ package stack;
 
 /**
  * @author Agnostic
- *ջ��ʵ��
+ *这是一个简单的堆栈，用java代码写的，利用泛型控制类型，有点递归的感觉吧，end()方法相当于末端哨兵，当读取到末端时，哨兵报警，停止读取。
+
+类的item相当于头，next相当于尾部。pop()方法负责拉出数据，push()方法负责推入数据。
+
+其实linkedList本身已经具备了创建堆栈所必须的方法，而stack可以通过两个泛型的类Stack可以通过两个泛型的类stack和linkedlist的组合来创建。
  */
 	import javax.naming.NameNotFoundException;
 	import java.awt.geom.Line2D;
